d376688dc3129e443ac2a95856fc04b8324aa013
Miles Williams <milesdwilliams15@gmail.com>
2017-01-08 19:23:16 -0600

    Updated analysis of term frequency by screen name

--- subset_names2.R
+++ subset_names2.R
@@ -44,7 +44,7 @@ ggplot(count, aes(x=reorder(screenName, freq), y = freq)) +
 length(unique(cruz$screenName)) #23
 length(cruz$screenName) #171
 
-bernie<- tweets[ grep("Bernie", tweets$text), ]
+sanders<- tweets[ grep("Sanders", tweets$text), ]
 count <- count(bernie, "screenName")
 ggplot(count, aes(x=reorder(screenName, freq), y = freq)) + 
   geom_bar(stat="identity") + coord_flip() + 
@@ -94,6 +94,35 @@ ggplot(count, aes(x=reorder(screenName, freq), y = freq)) +
 length(unique(rep$screenName)) #19
 length(rep$screenName) #86
 
+election<- tweets[ grep("election", tweets$text), ]
+
+trump$Mentions<-c("Trump")
+obama$Mentions<-c("Obama")
+clinton$Mentions<-c("Clinton")
+cruz$Mentions<-c("Cruz")
+sanders$Mentions<-c("Sanders")
+politics$Mentions<-c("politics")
+govt$Mentions<-c("government")
+dem$Mentions<-c("Democrat")
+rep$Mentions<-c("Republican")
+election$Mentions<-c("election")
+poli1<-rbind(trump,obama,clinton,cruz,sanders,politics,govt,dem,rep,election)
+count<-count(poli1,c("screenName","Mentions"))
+count1b <- count(poli1,c("screenName"))
+names<-count1b[with(count1b, order(freq,decreasing=FALSE)),]
+library(colorspace)
+pal <- choose_palette()
+ggplot(count, aes(x=reorder(screenName, freq), y = freq,fill=Mentions)) + 
+  geom_bar(stat="identity") + coord_flip() + scale_fill_manual(values=pal(10)) +
+  ggtitle("Number of Tweets Containing Mentions\nof Names of Politicians, Parties, Politics,\nand the Election") +
+  xlab("60 Screen Names") + ylab("Tweets = 1,633") +
+  theme_classic() +
+  scale_x_discrete(limits=names$screenName) +
+  theme(panel.grid.major.x=element_line(colour="grey50",linetype=3))
+length(unique(poli1$screenName)) #60
+length(poli1$screenName) #1633
+
+
 immig<- tweets[ grep("immigration", tweets$text), ]
 count <- count(immig, "screenName")
 ggplot(count, aes(x=reorder(screenName, freq), y = freq)) + 
@@ -123,6 +152,8 @@ ggplot(count, aes(x=reorder(screenName, freq), y = freq)) +
   theme_classic()
 length(unique(hom$screenName)) #6
 length(hom$screenName) #18
+
+homo<-tweets[grep("homosexual",tweets$text),]
 
 gay<- tweets[ grep("gay", tweets$text), ]
 count <- count(gay, "screenName")
@@ -183,3 +214,102 @@ ggplot(count, aes(x=reorder(screenName, freq), y = freq)) +
   theme_classic()
 length(unique(terrorism$screenName)) #15
 length(terrorism$screenName) #57
+
+muslim<-tweets[grep("Muslim",tweets$text),]
+tax<-tweets[grep("taxes",tweets$text),]
+econ<-tweets[grep("economy",tweets$text),]
+obamacare<-tweets[grep("Obamacare",tweets$text),]
+
+muslim$Mentions<-c("Muslim")
+tax$Mentions<-c("taxes")
+econ$Mentions<-c("economy")
+obamacare$Mentions<-c("Obamacare")
+terrorism$Mentions<-c("terrorism")
+jihad$Mentions<-c("jihad")
+isis$Mentions<-c("ISIS")
+islam$Mentions<-c("Islam")
+climate$Mentions<-c("climate change")
+gay$Mentions<-c("gay")
+homo$Mentions<-c("homosexual")
+hom$Mentions<-c("homosexuality")
+abortion$Mentions<-c("abortion")
+immig$Mentions<-c("immigration")
+poli2<-rbind(muslim,tax,econ,obamacare,terrorism,jihad,isis,islam,climate,
+             gay,homo,hom,abortion,immig)
+
+poli1$Type<-c("General")
+poli2$Type<-c("Specific Issues")
+
+poli<-rbind(poli1,poli2)
+
+poli35<-filter(poli,screenName == "lecrae"|screenName=="RonEdmondson"|
+                 screenName=="KirkCameron"|screenName=="MattChandler74"|
+                 screenName=="PastorJohnHagee"|screenName=="PastorChoco"|
+                 screenName=="Sarcasticluther"|screenName=="jamesmacdonald"|
+                 screenName=="bobrobertsjr"|screenName=="Chris_Hodges"|
+                 screenName=="drmoore"|screenName=="pastoremase"|
+                 screenName=="perrynoble"|screenName=="edstetzer"|
+                 screenName=="douglaswils"|screenName=="PastorMark"|
+                 screenName=="TedHaggard"|screenName=="DaveRamsey"|
+                 screenName=="rdland"|screenName=="MJGerson"|
+                 screenName=="DrJamesCDobson"|screenName=="RickWarren"|
+                 screenName=="aigkenham"|screenName=="ScottWilliams"|
+                 screenName=="JohnPiper"|screenName=="RevKevDeYoung"|
+                 screenName=="sstrang"|screenName=="albertmohler"|
+                 screenName=="raycomfort"|screenName=="Franklin_Graham"|
+                 screenName=="tperkins"|screenName=="ralphreed"|
+                 screenName=="DouthatNYT"|screenName=="DavidBartonWB"|
+                 screenName=="JaySekulow")
+               
+
+count<-count(poli,c("screenName","Type","Mentions"))
+count1b <- count(poli,c("screenName"))
+names<-count1b[with(count1b, order(freq,decreasing=FALSE)),]
+ggplot(count %>% filter(Type == "General"), aes(x=reorder(screenName, freq), y = freq,fill=Mentions)) + 
+  geom_bar(stat="identity") + coord_flip() + scale_fill_grey() +
+  ggtitle("Number of Tweets that Address Politics\nGenerally and Specific Political Issues") +
+  xlab("64 Screen Names") + ylab("Tweets = 2,697") +
+  theme_classic() +
+  scale_x_discrete(limits=names$screenName) +
+  theme(legend.justification=c(1,0),legend.position=c(1,0)) +
+  theme(panel.grid.major.x=element_line(colour="grey50",linetype=3))
+length(unique(poli$screenName)) #64
+length(poli$screenName) #2697
+
+count35<-count(poli35,c("screenName","Type","Mentions"))
+count1c <- count(poli35,c("screenName"))
+names35<-count1c[with(count1c, order(freq,decreasing=FALSE)),]
+ggplot(count35, aes(x=reorder(screenName, freq), y = freq,fill=Type)) + 
+  geom_bar(stat="identity") + coord_flip() + scale_fill_grey() +
+  ggtitle("Number of Tweets that Address Politics\nGenerally and Political Issues Specifically",
+          subtitle = "Top 35 Accounts among the 64 Accounts Identified") +
+  xlab("") + ylab("Total Tweets = 2,629") +
+  theme_classic() +
+  scale_x_discrete(limits=names35$screenName) +
+  theme(legend.justification=c(1,0),legend.position=c(1,0)) +
+  guides(fill=guide_legend(title=NULL)) +
+  theme(panel.grid.major.x=element_line(colour="grey50",linetype=3))
+length(poli35$screenName) #2629
+
+count1d <- count(poli,c("Mentions"))
+namesd<-count1d[with(count1d, order(freq,decreasing=FALSE)),]
+ggplot(count35, aes(x=reorder(Mentions, freq), y = freq,fill=Type)) + 
+  geom_bar(stat="identity") + coord_flip() + scale_fill_grey() +
+  ggtitle("Number of Political Tweets",
+          subtitle = "Selection of Terms Associated with Political Figures\nand Issues") +
+  xlab("") + ylab("Total Tweets = 2,629") +
+  theme_classic() +
+  scale_x_discrete(limits=namesd$Mentions) +
+  theme(legend.justification=c(1,0),legend.position=c(1,0)) +
+  guides(fill=guide_legend(title=NULL)) +
+  theme(panel.grid.major.x=element_line(colour="grey50",linetype=3))
+length(poli35$screenName) #2629
+
+
+names35<-c("lecrae","RonEdmondson","KirkCameron","MattChandler74","PastorJohnHagee",
+           "PastorChoco","Sarcasticluther","jamesmacdonald","bobrobertsjr","Chris_Hodges",
+           "drmoore","pastoremase","perrynoble","edstetzer","douglaswils","PastorMark",
+           "TedHaggard","DaveRamsey","rdland","MJGerson","DrJamesCDobson","RickWarren",
+           "aigkenham","ScottWilliams","JohnPiper","RevKevDeYoung","sstrang","albertmohler",
+           "raycomfort","Franklin_Graham","tperkins","ralphreed","DouthatNYT","DavidBartonWB",
+           "JaySekulow")
